f1df52eb1131cea5444ca904ef7d092b3900b909
Aleksander Konnerup <Alek.k@hotmail.com>
2018-09-11 17:04:43 +0200

    - Fået submit til at virke så collectionen opdateres - Forsøgt mig med population af viewet, når siden renderes, men virker ikke grundet en funktion den ikke kan finde. - Desuden har jeg ændret routing således at det hele løber igennem mainView.route (overflødiggør exercise.route.js, men tør ikke helt fjerne den endnu).

--- ITTWeb_Assignment1/app.js
+++ ITTWeb_Assignment1/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var mainViewRouter = require('./routes/mainView.route');
 var usersRouter = require('./routes/users');
 
-var Exercise = require('./routes/exercise.route')
+//var Exercise = require('./routes/exercise.route')
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', mainViewRouter);
 app.use('/users', usersRouter);
-app.use('/exercise', Exercise);
+//app.use('/exercise', Exercise);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
